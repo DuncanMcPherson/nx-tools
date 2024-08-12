e15127ef4b4d61d49c850fec9b3c507cbb4a9b81
Duncan McPherson <12081@rideuta.com>
2024-08-12 16:20:55 -0600

    working on tests

--- packages/nx-firebase/src/executors/firebase/executor.spec.ts
+++ packages/nx-firebase/src/executors/firebase/executor.spec.ts
@@ -2,8 +2,31 @@ import { ExecutorContext } from '@nx/devkit';
 
 import { FirebaseExecutorSchema } from './schema';
 import executor from './executor';
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import * as EventEmitter from "node:events";
+import * as stream from 'node:stream'
 
-const options: FirebaseExecutorSchema = {};
+
+jest.mock('node:child_process', () => ({
+	exec: jest.fn(() => {
+		const cp = new EventEmitter();
+		cp["stdout"] = new stream.Readable({
+			read () {
+				this.push("It is now safe to connect");
+				this.push(null);
+			}
+		});
+		cp["stderr"] = new stream.Readable();
+		return cp;
+	})
+}))
+
+const tree = createTreeWithEmptyWorkspace();
+
+const options: FirebaseExecutorSchema = {
+	cwd: tree.root,
+
+};
 const context: ExecutorContext = {
 	root: '',
 	cwd: process.cwd(),
