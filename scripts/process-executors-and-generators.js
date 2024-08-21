c972411dc5a5b0a90fb41f3313a1f941093606fb
Duncan McPherson <12081@rideuta.com>
2024-08-21 10:42:01 -0600

    chore(core): corrected processor script
    Should not repeatedly add dist to adjusted path

--- scripts/process-executors-and-generators.js
+++ scripts/process-executors-and-generators.js
@@ -1,31 +1,35 @@
 #!usr/bin/env node
 
-"use strict";
+'use strict';
 const devkit = require('@nx/devkit');
 const fs = require('fs');
 
 function processExecutorAndGenerators() {
-	console.log('Processing executors and generators...')
+	console.log('Processing executors and generators...');
 	if (fs.existsSync('./executors.json')) {
 		const executorsJson = devkit.readJsonFile('./executors.json');
 		Object.keys(executorsJson.executors).forEach((key) => {
 			const index = 1;
-			const implementationPathParts = executorsJson.executors[key].implementation.split("/");
-			const newArray = [
-				...implementationPathParts.slice(0, index),
-				'dist',
-				...implementationPathParts.slice(index)
-			];
-			executorsJson.executors[key].implementation = newArray.join('/');
-			console.log(`Implementation path for executor ${key} was updated to: ${newArray.join('/')}`)
+			const implementationPathParts = executorsJson.executors[key].implementation.split('/');
+			if (!implementationPathParts.includes('dist')) {
+				const newArray = [
+					...implementationPathParts.slice(0, index),
+					'dist',
+					...implementationPathParts.slice(index)
+				];
+				executorsJson.executors[key].implementation = newArray.join('/');
+				console.log(`Implementation path for executor ${key} was updated to: ${newArray.join('/')}`);
+			}
 			const schemaParts = executorsJson.executors[key].schema.split('/');
-			const newSchemaPath = [
-				...schemaParts.slice(0, index),
-				'dist',
-				...schemaParts.slice(index)
-			];
-			executorsJson.executors[key].schema = newSchemaPath.join('/');
-			console.log(`Schema path for executor ${key} was updated to: ${newArray.join('/')}`)
+			if (!schemaParts.includes('dist')) {
+				const newSchemaPath = [
+					...schemaParts.slice(0, index),
+					'dist',
+					...schemaParts.slice(index)
+				];
+				executorsJson.executors[key].schema = newSchemaPath.join('/');
+				console.log(`Schema path for executor ${key} was updated to: ${newSchemaPath.join('/')}`);
+			}
 		});
 		devkit.writeJsonFile('./executors.json', executorsJson);
 	}
@@ -34,23 +38,27 @@ function processExecutorAndGenerators() {
 		const generatorsJson = devkit.readJsonFile('./generators.json');
 		Object.keys(generatorsJson.generators).forEach((key) => {
 			const index = 1;
-			const factoryPathParts = generatorsJson.generators[key].factory.split("/");
-			const newArray = [
-				...factoryPathParts.slice(0, index),
-				'dist',
-				...factoryPathParts.slice(index)
-			];
-			const newFactoryPath = newArray.join('/');
-			generatorsJson.generators[key].factory = newFactoryPath;
-			console.log(`Factory path for generator ${key} was updated to: ${newFactoryPath}`)
+			const factoryPathParts = generatorsJson.generators[key].factory.split('/');
+			if (!factoryPathParts.includes('dist')) {
+				const newArray = [
+					...factoryPathParts.slice(0, index),
+					'dist',
+					...factoryPathParts.slice(index)
+				];
+				const newFactoryPath = newArray.join('/');
+				generatorsJson.generators[key].factory = newFactoryPath;
+				console.log(`Factory path for generator ${key} was updated to: ${newFactoryPath}`);
+			}
 			const schemaParts = generatorsJson.generators[key].schema.split('/');
-			const newSchemaPath = [
-				...schemaParts.slice(0, index),
-				'dist',
-				...schemaParts.slice(index)
-			];
-			generatorsJson.generators[key].schema = newSchemaPath.join('/');
-			console.log(`Schema path for generator ${key} was updated to: ${newArray.join('/')}`)
+			if (!schemaParts.includes('dist')) {
+				const newSchemaPath = [
+					...schemaParts.slice(0, index),
+					'dist',
+					...schemaParts.slice(index)
+				];
+				generatorsJson.generators[key].schema = newSchemaPath.join('/');
+				console.log(`Schema path for generator ${key} was updated to: ${newSchemaPath.join('/')}`);
+			}
 		});
 		devkit.writeJsonFile('./generators.json', generatorsJson);
 	}
