3f51a29e9806e3337bcbf0303568fc401123f4df
Duncan McPherson <12081@rideuta.com>
2024-09-03 11:20:54 -0600

    docs(firebase-cypress): added tooling for e2e testing
    added new project that will be considered a part of the requirements

---
+++ packages/firebase-cypress-e2e/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+	displayName: 'firebase-cypress-e2e',
+	preset: '../../jest.preset.js',
+	transform: {
+		'^.+\\.[tj]s$': [
+			'ts-jest',
+			{ tsconfig: '<rootDir>/tsconfig.spec.json' },
+		],
+	},
+	moduleFileExtensions: ['ts', 'js', 'html'],
+	coverageDirectory: '../../coverage/packages/firebase-cypress-e2e',
+	globalSetup: '..\\..\\tools\\scripts\\start-local-registry.ts',
+	globalTeardown: '..\\..\\tools\\scripts\\stop-local-registry.ts',
+};
