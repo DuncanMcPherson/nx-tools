92afc317884cccf31f4c09dbcfa9f7f44ec0606e
Duncan McPherson <12081@rideuta.com>
2024-09-09 21:54:12 -0600

    starting work on init e2e

--- packages/firebase-cypress-e2e/src/firebase-cypress.spec.ts
+++ packages/firebase-cypress-e2e/src/firebase-cypress.spec.ts
@@ -1,37 +1,46 @@
 import { execSync } from 'child_process';
 import { join, dirname } from 'path';
-import { mkdirSync, rmSync } from 'fs';
+import { mkdirSync, rmSync, writeFileSync } from 'fs';
 
 describe('firebase-cypress', () => {
-	let projectDirectory: string;
+  let projectDirectory: string;
 
-	beforeAll(() => {
-		projectDirectory = createTestProject();
+  beforeAll(() => {
+    projectDirectory = createTestProject();
 
-		// The plugin has been built and published to a local registry in the jest globalSetup
-		// Install the plugin built with the latest source code into the test repo
-		execSync(`npm install @nxextensions/firebase-cypress@e2e`, {
-			cwd: projectDirectory,
-			stdio: 'inherit',
-			env: process.env,
-		});
-	});
+    // The plugin has been built and published to a local registry in the jest globalSetup
+    // Install the plugin built with the latest source code into the test repo
+    execSync(`npm install @nxextensions/firebase-cypress@e2e`, {
+      cwd: projectDirectory,
+      stdio: 'inherit',
+      env: process.env,
+    });
+  });
 
-	afterAll(() => {
-		// Cleanup the test project
-		rmSync(projectDirectory, {
-			recursive: true,
-			force: true,
-		});
-	});
+  afterAll(() => {
+    // Cleanup the test project
+    if (projectDirectory) {
+      rmSync(projectDirectory, {
+        recursive: true,
+        force: true,
+      });
+    }
+  });
 
-	it('should be installed', () => {
-		// npm ls will fail if the package is not installed properly
-		execSync('npm ls @nxextensions/firebase-cypress', {
-			cwd: projectDirectory,
-			stdio: 'inherit',
-		});
-	});
+  it('should be installed', () => {
+    // npm ls will fail if the package is not installed properly
+    execSync('npm ls @nxextensions/firebase-cypress', {
+      cwd: projectDirectory,
+      stdio: 'inherit',
+    });
+  });
+
+  it('should create the e2e project properly', () => {
+    rmSync(join(projectDirectory, 'test-project-e2e'), {
+      recursive: true,
+      force: true,
+    });
+  });
 });
 
 /**
@@ -39,27 +48,49 @@ describe('firebase-cypress', () => {
  * @returns The directory where the test project was created
  */
 function createTestProject() {
-	const projectName = 'test-project';
-	const projectDirectory = join(process.cwd(), 'tmp', projectName);
+  const projectName = 'test-project';
+  const projectDirectory = join(process.cwd(), 'tmp', projectName);
 
-	// Ensure projectDirectory is empty
-	rmSync(projectDirectory, {
-		recursive: true,
-		force: true,
-	});
-	mkdirSync(dirname(projectDirectory), {
-		recursive: true,
-	});
+  // Ensure projectDirectory is empty
+  rmSync(projectDirectory, {
+    recursive: true,
+    force: true,
+  });
+  mkdirSync(dirname(projectDirectory), {
+    recursive: true,
+  });
 
-	execSync(
-		`npx --yes create-nx-workspace@latest ${projectName} --preset apps --nxCloud=skip --no-interactive`,
-		{
-			cwd: dirname(projectDirectory),
-			stdio: 'inherit',
-			env: process.env,
-		}
-	);
-	console.log(`Created test project in "${projectDirectory}"`);
+  execSync(
+    `npx --yes create-nx-workspace@latest ${projectName} --preset angular-monorepo --ssr false --nxCloud=skip --appName ${projectName} --bundler esbuild --style scss --e2eTestRunner cypress`,
+    {
+      cwd: dirname(projectDirectory),
+      stdio: 'inherit',
+      env: process.env,
+    }
+  );
+  console.log(`Created test project in "${projectDirectory}"`);
 
-	return projectDirectory;
+  convertAppToFirebaseApp(join(projectDirectory, 'apps', projectName));
+
+  return projectDirectory;
+}
+
+function convertAppToFirebaseApp(projectRoot: string): void {
+  const firebaseJson = {
+    emulators: {
+      auth: {
+        port: 9099,
+      },
+      database: {
+        port: 9000,
+      },
+      singleProjectMode: true,
+      ui: {
+        enabled: true,
+      },
+    },
+  };
+  const firebaseJsonPath = join(projectRoot, 'firebase.json');
+  writeFileSync(firebaseJsonPath, JSON.stringify(firebaseJson));
+  console.log(`Created firebase.json configuration at: ${firebaseJsonPath}`);
 }
