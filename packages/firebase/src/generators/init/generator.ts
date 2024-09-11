079384fa930e274fb89b73e56f88b7dc103184ce
Duncan McPherson <12081@rideuta.com>
2024-09-11 10:26:35 -0600

    feat(firebase): completed firebase init generator

--- packages/firebase/src/generators/init/generator.ts
+++ packages/firebase/src/generators/init/generator.ts
@@ -1,5 +1,8 @@
 import {
   createProjectGraphAsync,
+  formatFiles,
+  generateFiles,
+  joinPathFragments,
   ProjectConfiguration,
   ProjectGraph,
   readNxJson,
@@ -7,6 +10,7 @@ import {
   Tree,
 } from '@nx/devkit';
 import { addPlugin } from '@nx/devkit/src/utils/add-plugin';
+import { join } from 'path';
 import { createNodesV2 } from '../../targets';
 
 export async function initGenerator(tree: Tree) {
@@ -22,6 +26,7 @@ export async function initGenerator(tree: Tree) {
 
   const projects = readProjectsConfigurationFromProjectGraph(graph).projects;
   await generateConfigurationFiles(tree, projects);
+  await formatFiles(tree);
 }
 
 async function addFirebasePlugin(
@@ -50,7 +55,30 @@ async function generateConfigurationFiles(
     if (!hasFirebaseJson(configuration.root, tree)) {
       return;
     }
+
+    let targetName: string;
+    if (!configuration.targets['serve']) {
+      Object.keys(configuration.targets).forEach((key) => {
+        if (!targetName && key.toLowerCase().includes('serve')) {
+          targetName = key;
+        }
+      });
+    } else {
+      targetName = 'serve';
+    }
+
+    generateFiles(tree, join(__dirname, 'files'), configuration.root, {
+      project: name,
+      target: targetName,
+      tmpl: '',
+    });
   });
+}
+
+function hasFirebaseJson(projectRoot: string, tree: Tree): boolean {
+  return tree.exists(
+    joinPathFragments(tree.root, projectRoot, 'firebase.json')
+  );
 }
 
 export default initGenerator;
